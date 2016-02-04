af20d11b0cb83cf72df6365a22da2d4c13c13262
DataNick <DataNick@users.noreply.github.com>
2016-02-03 19:36:23 -0500

    Added routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+//Dependencies
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+//MongoDB
+mongoose.connect('mongodb://localhost/rest_test');
+
+//Express
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Routes
+app.use('/api', require('./routes/api'));
 
 //This is not an API - it is static
-app.get('/', function (req, res){
-  res.send('This is working');
+// app.get('/', function (req, res){
+//   res.send('This is working');
+// });
 
-});
-
+//Start server
 app.listen('3000');
 console.log('API is running on port 3000!');
