6dd69de343f5f3316105233c8dfdd08b3197e2fa
DataNick <DataNick@users.noreply.github.com>
2016-02-03 19:22:05 -0500

    Checked to see if nodemon worked when making change to server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
+//This is not an API - it is static
 app.get('/', function (req, res){
   res.send('This is working');
 
 });
 
 app.listen('3000');
-console.log('API is running on port 3000');
+console.log('API is running on port 3000!');
